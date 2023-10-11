0a2d98541103fbcc832206d2e779cf713639b78e
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2023-10-11 11:32:09 +0800

    Day_3

---
+++ aritmatika.java
@@ -0,0 +1,21 @@
+public class operator_aritmatika {
+    public static void main(String[] args) {
+        int angka1 = 18;
+        int angka2 = 29;
+
+        int Penjumlahan = angka1 + angka2;
+        System.out.println("Penjumlahan: " + Penjumlahan);
+        
+        int Pengurangan = angka1 - angka2;
+        System.out.println("Pengurangan: " + Pengurangan);
+
+        int Perkalian = angka1 * angka2;
+        System.out.println("Perkalian: " + Perkalian);
+        
+        int Pembagian = angka1 / angka2;
+        System.out.println("Pembagian: " + Pembagian);
+
+        int sisaBagi = angka1 % angka2;
+        System.out.println("Sisa hasil bagi: " + sisaBagi);
+    }
+}
