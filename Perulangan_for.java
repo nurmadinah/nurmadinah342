9e50633afeefa9c903831de94179ce74098d0afa
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2023-10-12 14:16:21 +0800

    Day_4

--- Perulangan_for.java
+++ Perulangan_for.java
@@ -3,10 +3,8 @@ public class Perulangan_for {
     public static void main(String[] args) {
    
         for (int i = 1; i < 10; i++) {
-             System.out.println("Informatics Study Club");
-             
+             System.out.println("Informatics Study Club");    
              System.out.println("Sintia");
-             
         }
     }
 }
