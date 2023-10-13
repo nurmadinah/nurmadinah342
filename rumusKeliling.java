d7b5f29c71d44027c828a18455e89f3d7eff8979
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2023-10-13 20:47:07 +0800

    Day_5

---
+++ rumusKeliling.java
@@ -0,0 +1,16 @@
+
+public class hari5 {
+    public static void main(String[] args) {
+        int panjang=23;
+        int lebar=40;
+        
+        
+        int rumus = panjang*lebar;
+        int kel = 2*(panjang+lebar);
+        System.out.println("luas    :"+rumus+"cm");
+        System.out.println("Keliling:"+kel+"cm");
+        
+        
+        
+    }
+}
