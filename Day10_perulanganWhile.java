89eff161a5d50a26c72760abb065da0cbb63d2de
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2023-10-18 20:49:31 +0800

    Day_10

---
+++ Day10_perulanganWhile.java
@@ -0,0 +1,10 @@
+public class do_while {
+    public static void main(String[] args) {
+        int Nomor = 1;
+            do {
+                System.out.println("Nomor:" + Nomor);
+                Nomor++;
+                }
+            while (Nomor <= 100);
+    }
+}
