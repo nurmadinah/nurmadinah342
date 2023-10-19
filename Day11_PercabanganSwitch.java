9247cb48046efce9201b472a36546f2528477916
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2023-10-19 21:00:08 +0800

    Day_11

---
+++ Day11_PercabanganSwitch.java
@@ -0,0 +1,15 @@
+public class Percabangan_switch {
+    public static void main(String[] args) {
+        int pilihan = 200;
+    switch (pilihan) {
+        case 100:
+            System.out.println("Kamu Memilih Pilihan pertama Yaitu 100");
+            break;
+        case 200:
+            System.out.println("Kamu Memilih Pilihan Kedua Yaitu 200");
+            break;
+        default:
+            System.out.println("Tidak Ada yang terpilih");
+        }
+    }
+}
