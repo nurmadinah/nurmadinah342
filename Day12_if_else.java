19a0d27c9b356488f5cd7e7fa4211bf44cbabca4
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2023-10-20 22:13:50 +0800

    Day_12

---
+++ Day12_if_else.java
@@ -0,0 +1,17 @@
+public class percabangan_if_else {
+    public static void main(String[] args) {
+        int nilai = 90;
+        if (nilai >= 80) {
+            System.out.println("Nilai : A");
+        }
+         else if (nilai >= 75) {
+            System.out.println("Nilai : B");
+        }
+         else if (nilai >= 65) {
+            System.out.println("Nilai : C");
+        }
+         else {
+            System.out.println("Nilai : D");
+        }
+    }
+}
