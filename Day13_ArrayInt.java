8c5de9c849e3aac22800978309d76feacf4d53cf
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2023-10-21 21:18:50 +0800

    Day_13

---
+++ Day13_ArrayInt.java
@@ -0,0 +1,12 @@
+public class array {
+    public static void main(String[] args) {
+        int[] angka = new int[5]; // Membuat array integer dengan 5 elemen
+        angka[0] = 10;
+        angka[1] = 20;
+        angka[2] = 30;
+
+
+        int nilai = angka[2]; // Akan menghasilkan 30
+        System.out.println("Nilai ketiga Dalam Array Adalah:"+nilai);
+    }
+}
