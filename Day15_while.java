24f37409e026cf901d452eb7a913e88afb075ed2
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2023-10-23 20:11:19 +0800

    Day_15

---
+++ Day15_while.java
@@ -0,0 +1,14 @@
+package subhan;
+
+public class perulangan_while {
+
+    public static void main(String[] args) {
+        int s=1;
+        
+        while(s<=10){
+            System.out.println("no."+s);
+            s++;
+        }
+        System.out.println("hitungan selesai terimakasih");
+    }
+}
