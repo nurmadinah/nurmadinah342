b92d0f026d71eb7e34f4aa3a7315177a61510780
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2023-10-25 20:53:07 +0800

    Day_17

---
+++ Day17_LinkedList.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+public class Days17_LingkedList {
+
+ 
+    public static void main(String[] args) {
+        LinkedList <Integer> a = new LinkedList<>();
+        a.add(20);
+        a.add(30);
+        a.add(40);
+      
+       
+        int nilai = a.getFirst();
+        System.out.println("Nilai Yang Diambil Adalah: "+ nilai);
+    }
+    
+}
