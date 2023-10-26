fe060e8c5380d0cae81325585c6c5bac16f96fa2
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2023-10-26 21:47:03 +0800

    Day_18

---
+++ Day18_loopDoWhile.java
@@ -0,0 +1,13 @@
+package fileaa;
+
+
+public class loop_do_while {
+    public static void main(String[] args) {
+        int q = 1;
+        do {
+            System.out.print(q+" ");
+            q++;
+        }while (q<=5);
+        
+    }
+}
