2481836a185ea1929f414e16cde52f1480faaeb2
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2023-10-28 21:12:22 +0800

    Day_20

---
+++ Day20_ArraySort.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class struktur_array_sort {
+    public static void main(String[] args) {
+        int[] nilai = {3,7,12,10, 5, 8, 9, 1,80,2,2839920,-4};
+        Arrays.sort(nilai);
+        System.out.println("Array setelah diurutkan: " + Arrays.toString(nilai));
+    }
+}
