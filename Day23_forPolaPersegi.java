56f64e1358be35ec03afdad18c36952c74b0d612
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2023-10-31 21:11:13 +0800

    Day_23

---
+++ Day23_forPolaPersegi.java
@@ -0,0 +1,13 @@
+
+public class for_polapersegi {
+    public static void main(String[] args) {
+        int pola = 10;
+        for (int i =0; i<pola;i++){
+            for(int j=0; j<pola;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+}
