2a9c3f024dbf9ee638689232a8ded5a4af9e3c49
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2023-11-04 07:51:43 +0800

    Day_26

---
+++ Day26_menghitungkecepatan.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public static void main(String[] args) {
+
+String Jaraktempuh = null;
+String Waktutempuh = null;
+try {
+System.out.print("Masukkan Jarak Tempuh(km) :");
+Jaraktempuh = in.readLine();
+
+System.out.print("Masukkan Waktu Tempuh (jam) :");
+Waktutempuh = in.readLine();
+
+} catch(IOException ioe) {
+System.out.println();
+}
+double JarakTempuh;
+double WaktuTempuh;
+
+JarakTempuh = Double.parseDouble(Jaraktempuh);
+WaktuTempuh = Double.parseDouble(Waktutempuh);
+Terakhir kita ubah rumus matematika dari bunga tunggal ke bentuk bahasa pemrograman
+double Kecepatan = JarakTempuh/WaktuTempuh;
+System.out.println("Kecepatan : "+ Kecepatan);
+Kemudian buat batas
+int kecepatan = 40;
+if (kecepatan > (threshold+1)) {
+System.out.print(String.format("kecepatan : %d lebih dari: %d maka laju kendaraan cepat", kecepatan, threshold));
+} else if (kecepatan == 40) {
+System.out.print(String.format("kecepatan : %d sama dengan: %d maka laju kendaraan biasa", kecepatan, threshold));
+} else {
+System.out.print(String.format("kecepatan : %d kurang dari: %d maka laju kendaraan lambat", kecepatan, threshold));
+}
+}
+}
