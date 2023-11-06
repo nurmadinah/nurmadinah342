7015602290b13ad5f0d36b73efba53f796d08251
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2023-11-06 09:43:55 +0800

    Day_29

---
+++ Day29_Double.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    // membuat variabel int
+    int a =33;
+    int b = 29;
+    // ubah int menjadi double
+    double c = a;
+    double d = b;
+
+    System.out.println(c);    // 33.0
+    System.out.println(d);    // 29.0
+  }
+}
