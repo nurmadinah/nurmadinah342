32b1a194433b5a8d36518fb5006533e46ee31473
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2023-11-07 17:47:52 +0800

    Day_30

---
+++ day30_tipeDataBoolean.java
@@ -0,0 +1,14 @@
+class day30 {
+  public static void main(String args[]){
+        
+    boolean var1, var2, var3;
+ 
+    var1 = 12 < 10;
+    var2 = 30 > 25;
+    var3 = 'A' == 'a';
+     
+    System.out.println("var1 = "+var1);
+    System.out.println("var2 = "+var2);  
+    System.out.println("var3 = "+var3);  
+  }
+}
