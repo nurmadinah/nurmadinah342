470ed67d5774a18b782710cf3932ee8191157793
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2023-11-09 18:31:04 +0800

    Day_32

---
+++ day32_TipedataChar.java
@@ -0,0 +1,10 @@
+class day32 {
+  public static void main(String args[]){
+        
+    char var1 = '5';
+    char var2 = '$';
+     
+    System.out.println(var1);
+    System.out.println(var2);
+  }
+}
