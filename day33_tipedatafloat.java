c72a75853519865b3647d2928a1f66dd9834e8bc
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2023-11-10 18:42:15 +0800

    Day_33

---
+++ day33_tipedatafloat.java
@@ -0,0 +1,9 @@
+public class TipeDataFloat{
+  public static void main(String[] args) {
+    float a = 22;
+    float b = 7;
+    float hasil = a / b;
+
+    System.out.printf("hasil dari %.2f / %.2f adalah %.2f", a, b, hasil);
+  }
+}
