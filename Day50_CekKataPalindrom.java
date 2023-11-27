6610888e1eb2b4bc5117302c6d3a2e5d1d87dd70
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2023-11-27 21:04:19 +0800

    Day_50

---
+++ Day50_CekKataPalindrom.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class CekKataPalindrom {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan sebuah kata: ");
+
+        String kata = scanner.nextLine();
+
+        // Memanggil metode untuk menentukan apakah kata tersebut palindrom
+
+        boolean Palindrome = kata.equals(new StringBuilder(kata).reverse().toString())
+
+
+        if (Palindrome) {
+
+            System.out.println("Kata \"" + kata + "\" adalah palindrom.");
+
+        } else {
+
+            System.out.println("Kata \"" + kata + "\" bukan palindrom.");
+
+        }
+
+    }
+
+}
