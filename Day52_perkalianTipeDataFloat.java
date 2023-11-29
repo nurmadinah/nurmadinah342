838c3135c08d17af40282701e8f71cbbacd9dcd5
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2023-11-29 21:30:02 +0800

    Day_52

---
+++ Day52_perkalianTipeDataFloat.java
@@ -0,0 +1,12 @@
+
+
+public class perkalian_float {
+    public static void main(String[] args) {
+        float per=89;
+        float pus=35;
+        
+        float perpus=per*pus;
+        
+        System.out.println("Hasil perkalian tipe data float: "+perpus);
+    }
+}
