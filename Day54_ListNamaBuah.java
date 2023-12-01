8f07436dd9626f996fde0b61a445aef12322270c
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2023-12-01 20:14:04 +0800

    Day_54

---
+++ Day54_ListNamaBuah.java
@@ -0,0 +1,17 @@
+public class days54 {
+    public static void main(String[] args) {
+        //deklarasi variabel String
+        String buah1 = "apel";
+        String buah2 = "semangka";
+        String buah3 = "anggur";
+        
+        System.out.println("buah 1: "+buah1);//menampilkan buah 1
+        System.out.println("buah 2: "+buah2);//menampilkan buah 2
+        System.out.println("buag 3: "+buah3);//menampilkan buah 3
+        
+        //menggabungkan  3 nama buah menjadi 1 string
+        String semuaBuah = buah1 +","+buah2+ ",dan "+buah3;
+        System.out.println("semua buah: "+semuaBuah);//menampilkan semua buah
+        
+    }
+}
