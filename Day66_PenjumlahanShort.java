3ed86c2d794362bae02378eebfce0243139f9b4e
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2023-12-13 22:43:25 +0800

    Day_66

---
+++ Day66_PenjumlahanShort.java
@@ -0,0 +1,14 @@
+
+
+public class day66nonprimitif_penjumlahanShort {
+    public static void main(String[] args) {
+        Short suk = 76;
+        Short ses= 24;
+
+        
+        Short sukses = (short) (suk + ses);
+
+        
+        System.out.println("Hasil penjumlahan Short: " + sukses);
+    }
+}
