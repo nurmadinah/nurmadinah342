8868e0305d4ceb6f33a0f1add98f5ca2540eaa83
NURMADINAH <99579021+nurmadinah@users.noreply.github.com>
2023-12-14 20:07:40 +0800

    Day_67

---
+++ Day67_PerulanganForEachInteger.java
@@ -0,0 +1,11 @@
+public class fitra {
+    
+    public static void main(String[] args) {
+        int angka[] = {0, 541, 265, 563, 465};
+
+        for (int x : angka) {
+            System.out.print(x + " ");
+        }
+    }
+    
+}
