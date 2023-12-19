633895c43542f24cc7f4c09512ea5453cdee00bf
NURMADINAH342 <99579021+nurmadinah342@users.noreply.github.com>
2023-12-19 22:01:46 +0800

    Dau_72

---
+++ Days72_MenghitungLuasLingkaran.java
@@ -0,0 +1,17 @@
+
+
+
+public class Days72{
+
+    public static void main(String[] args) {
+
+    final double PI = 3.14;
+
+ 
+        double jariJari = 5.0;
+double Lingkaran = PI * jariJari * jariJari;
+        String luasLingkaran = null;
+
+        System.out.println("Luas Lingkaran dengan jari-jari " + jariJari + " adalah: " + luasLingkaran);
+    }
+}
