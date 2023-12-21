a85213e589cab7ad0210a73ce17432574b5c04b0
NURMADINAH342 <99579021+nurmadinah342@users.noreply.github.com>
2023-12-21 23:39:43 +0800

    Day_74

---
+++ Day74_ScannerDouble.java
@@ -0,0 +1,26 @@
+
+public class opertorAritmatika_Scanner_double {
+
+    public static void main(String[] args) {
+
+        double a,b;
+
+        Scanner ar = new Scanner (System.in);
+
+        System.out.print("masukkan nilai A : ");
+
+        a=ar.nextDouble();
+
+         System.out.print("masukkan nilai B : ");
+
+         b=ar.nextDouble();
+
+         System.out.println("Hasil : "+(a+b));
+
+
+
+    }
+
+    
+
+}
