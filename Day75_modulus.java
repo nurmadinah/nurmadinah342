0a1fac64e6c1bc45550dccd4f0f36d2d03a832f0
NURMADINAH342 <99579021+nurmadinah342@users.noreply.github.com>
2023-12-23 23:34:48 +0800

    Day_76

---
+++ Day75_modulus.java
@@ -0,0 +1,11 @@
+
+public class day76nonprimitif_modulus_Integer {
+    public static void main(String[] args) {
+        Integer mod=432;
+        Integer ulus=3;
+        
+        Integer modulus=mod%ulus;
+        
+        System.out.println("modulus Nonprimitif Integer: "+modulus);
+    }
+}
