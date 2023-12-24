8bbab9d01b84f1bebaa8c0af68addc1ce51a4ca0
NURMADINAH342 <99579021+nurmadinah342@users.noreply.github.com>
2023-12-24 20:20:05 +0800

    Day_77

---
+++ Day77_scannerShort.java
@@ -0,0 +1,12 @@
+
+public class tipeDataNonPrimitifShort {
+    public static void main(String[] args) {
+        
+        Short nilai = 25;
+        //mencetak nilai variabel non primitif ketipe data short
+        System.out.println("nilai variabel short: "+nilai);
+        ///mengubah nilai variabel non primitif tipe data short
+        nilai = 30;
+        System.out.println("nilai variabel short yang diubah: "+nilai);
+    }
+}
