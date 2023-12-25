c361b1cfd01cd9ce7e0f1d3ccf03dbfc2515b6f4
NURMADINAH342 <99579021+nurmadinah342@users.noreply.github.com>
2023-12-25 22:40:30 +0800

    Day_78

---
+++ Day78_tipeChar.java
@@ -0,0 +1,10 @@
+
+
+public class day78nonprimitif_Character {
+    public static void main(String[] args) {
+        Character aku='A';
+        
+        
+        System.out.println("Nilai character aku adalah : "+aku );
+    }
+}
