85722b927b2096669687203bd2e1676d1f979338
NURMADINAH342 <99579021+nurmadinah342@users.noreply.github.com>
2023-12-26 21:55:32 +0800

    Day_79

---
+++ Day79.perkalianMatrix.java
@@ -0,0 +1,24 @@
+public class madinah {
+    public static void main(String[] args) {
+      
+        int[][] A = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] B = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        int[][] C = new int[A.length][B[0].length];
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B[0].length; j++) {
+                for (int k = 0; k < A[0].length; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+
+        for (int i = 0; i < C.length; i++) {
+            for (int j = 0; j < C[0].length; j++) {
+                System.out.print(C[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
