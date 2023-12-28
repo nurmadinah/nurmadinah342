2ece7e47998b9ac41be6d2857d39bc052fec1d70
NURMADINAH342 <99579021+nurmadinah342@users.noreply.github.com>
2023-12-28 20:46:21 +0800

    Day_81p

---
+++ Day81_MengambilElemenArray.java
@@ -0,0 +1,13 @@
+public class dinah {
+    public static void main(String[] args) {
+        
+        //mendeklarasikan array nilai dengan 5 elemen 
+        int[] nilai = {12,15,20,10,8};
+        
+        //membuat variabel angka untuk menyimpan nilai elemen ke3 dari array 
+        int angka = nilai[3];
+        
+        //mencetak  variabel angka
+        System.out.println("angka: "+angka);
+    }
+}
