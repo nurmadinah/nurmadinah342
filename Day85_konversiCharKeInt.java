ab9aaf4f1cea78991e3e9498b12f6bb2136c732c
NURMADINAH342 <99579021+nurmadinah342@users.noreply.github.com>
2024-01-01 20:53:32 +0800

    Day_85

---
+++ Day85_konversiCharKeInt.java
@@ -0,0 +1,11 @@
+
+public class days83 {
+    public static void main(String[] args) {
+        //mengonversi nilai char ke integer
+        int i = (int)'A';
+        System.out.println("i = " + i);
+       
+        char c = (char)65;
+        System.out.println("c = "+ c);
+    }
+}
