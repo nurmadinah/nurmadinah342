5f0c328fbc1a58e429e42cf92c0b979c616cfb1a
NURMADINAH342 <99579021+nurmadinah342@users.noreply.github.com>
2024-01-10 20:56:51 +0800

    Day_94

---
+++ Day94_perkalianBulanganBulat.java
@@ -0,0 +1,9 @@
+public class dinah {
+    public static void main(String[] args) {
+        int a = 75;//DEKLARASI variabel
+        int b = 25;
+        int hasil = a*b;//perkalian
+        System.out.println("hasil perkalian " + a + " dan " + b + " adalah " + hasil);
+    }
+ 
+}
