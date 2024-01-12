83cdfd56d64f09fc2ccc901c066c0d5b3577dc05
NURMADINAH342 <99579021+nurmadinah342@users.noreply.github.com>
2024-01-12 21:31:43 +0800

    Day_96

---
+++ Day96_arrayListAngka.java
@@ -0,0 +1,17 @@
+
+
+public class Dinah {
+
+    public static void main(String[] args) {
+
+        int[] angka = {1, 2, 3, 4, 5};
+
+        for (int nilai : angka) {
+
+            System.out.println(nilai);
+
+        }
+
+    }
+
+}
